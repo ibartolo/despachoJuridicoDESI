cd6b614808e5ae42dd37b6bbe41de56590c2a271
ibartolo <bartolocastro@gmail.com>
2026-02-09 15:54:52 -0600

    Se agrega funcionalidad para poder mostrar en razor la información del caso, se agrega funcionalidad para poder mostrar la vista del Cliente

--- DespachoJuridicoDESIMVC/Controllers/CaseController.cs
+++ DespachoJuridicoDESIMVC/Controllers/CaseController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using DespachoJuridicoDESIMVC.Models.Case;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using static DespachoJuridicoDESIMVC.Helpers.FiltersHelper;
@@ -8,12 +12,39 @@ using static DespachoJuridicoDESIMVC.Helpers.FiltersHelper;
 namespace DespachoJuridicoDESIMVC.Controllers
 {
     [Autenticated]
-    public class CaseController : Controller
+    public class CaseController : BaseController
     {
         // GET: Case
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var response = await httpClient.GetAllClientes();
+
+            var listItemsCleintes = new List<SelectListItem>();
+            var listItemsEstatus = new List<SelectListItem>();
+
+            foreach (var i in response.ClientObjs)
+            { 
+                listItemsCleintes.Add(new SelectListItem
+                {
+                    Text = i.Nombre,
+                    Value = i.Id.ToString()
+                });
+            }
+            listItemsEstatus.Add(new SelectListItem
+            {
+                Text = "Abierto",
+                Value = "1"
+            });
+            listItemsEstatus.Add(new SelectListItem
+            {
+                Text = "Cerrado",
+                Value = "2"
+            });
+
+
+            ViewBag.EstatusCaso = listItemsEstatus;
+            ViewBag.Clientes = listItemsCleintes;
+            return View(new CaseObj());
         }
     }
 }
---
+++ DespachoJuridicoDESIMVC/Models/Case/StatusCaseObj.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DespachoJuridicoDESIMVC.Models.Case
+{
+    public class StatusCaseObj
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Orden { get; set; }
+        public bool GeneraEvento { get; set; }
+        public bool GeneraNotificacion { get; set; }
+        public bool Estatus { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedDt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedDt { get; set; }
+
+    }
+}
