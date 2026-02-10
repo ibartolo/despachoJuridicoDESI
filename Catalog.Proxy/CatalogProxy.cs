77c06bfed0801c453da4f091c9c27e70ef6001b3
ibartolo <bartolocastro@gmail.com>
2026-02-10 16:35:19 -0600

    Se agrega catalogo proxy

---
+++ Catalog.Proxy/CatalogProxy.cs
@@ -0,0 +1,13 @@
+﻿using SqlProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Proxy
+{
+    public class CatalogProxy: DbWrapper, ICatalogProxi
+    {
+    }
+}
